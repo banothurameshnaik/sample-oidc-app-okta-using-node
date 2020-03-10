d4b3a4227c062a997b95302ae1c9aaefa39f0502
Banothu Ramesh Naik <rameshnaik5521@gmail.com>
2020-03-10 16:57:21 +0530

    Port added to env

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 var bodyParser = require('body-parser');
 
@@ -8,7 +9,7 @@ const {
 } = require('./handlers.js');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
